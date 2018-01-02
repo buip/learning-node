f0fa0ec7b60f1ee67d63dba9f36f87a5977e9425
Phuong Bui <buip@beloit.edu>
2018-01-02 18:28:59 +0700

    Register helper and partial for reusable code

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const hbs = require('hbs');
 
 let app = express();
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 
 app.get('/', (req, res) => {
 	res.render('home.hbs', {
