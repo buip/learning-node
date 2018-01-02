04f03c1b47d7a33dd46e0a4917f2b01b9644596a
Phuong Bui <buip@beloit.edu>
2018-01-02 21:58:13 +0700

    Add maintainance page using express middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const hbs = require('hbs');
 
@@ -6,6 +7,25 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+	next();
+});
+
+app.use((req, res, next) => {
+	res.render('maintainance.hbs', {
+		pageTitle: 'Maintainance',
+		maintainanceMessage: 'The site is being updated'
+	});
+});
+
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 
 app.get('/', (req, res) => {
@@ -13,7 +33,7 @@ app.get('/', (req, res) => {
 		pageTitle: 'Home Page',
 		welcomeMessage: 'Welcome to our site',
 		currentYear: new Date().getFullYear()
-	})
+	});
 });
 
 app.get('/about', (req, res) => {
