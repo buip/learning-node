52cc91eab985f9135f3f215bd4048f357c56358e
buip <prestonbui97@gmail.com>
2018-01-04 11:25:13 +0700

    ObjectId

--- to-do-api/playground/mongodb-connect.js
+++ to-do-api/playground/mongodb-connect.js
@@ -1,6 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
-
-const dbName = 'TodoApp';
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
   if (err) {
@@ -26,7 +25,7 @@ MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
       if (err) {
         return console.log('Unable to insert todo', err);
       }
-      console.log(JSON.stringify(result.ops, undefined, 2));
+      console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
     });
 
     client.close();
